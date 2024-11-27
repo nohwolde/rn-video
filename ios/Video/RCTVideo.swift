a67a2b97aa410250a6ff835457e5077b42459435
nohwolde <58862403+nohwolde@users.noreply.github.com>
2024-11-27 14:13:23 -0500

    more updates

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -142,12 +142,12 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
 
         nowPlayingManager.setNextTrackHandler { [weak self] in
             guard let self = self else { return }
-            self._onSkipToNext()
+            self._onSkipToNext?()
         }
 
         nowPlayingManager.setPreviousTrackHandler { [weak self] in
             guard let self = self else { return }
-            self._onPlayPrevious()
+            self._onPlayPrevious?()
         }
 
         nowPlayingManager.registerCommandTargets()
